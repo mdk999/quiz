2e744f0eb8a753a019d67f49730216884d4fa0e7
rostykamga <rostykamga@gmail.com>
2015-03-13 06:13:49 +0100

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -14,27 +14,27 @@ public class Parser {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream fis = new FileInputStream(file);
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    int byteRead;
+    while ((byteRead = fis.read()) > 0) {
+      output += (char) byteRead;
     }
     return output;
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    FileInputStream fis = new FileInputStream(file);
     String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    int byteRead;
+    while ((byteRead = fis.read()) > 0) {
+      if (byteRead < 0x80) {
+        output += (char) byteRead;
       }
     }
     return output;
   }
   public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+    FileOutputStream fos = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
       o.write(content.charAt(i));
     }
